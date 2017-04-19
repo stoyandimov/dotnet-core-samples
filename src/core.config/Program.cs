1eaf84d5ad2d62ac2799465e91e4c98e3a114265
Stoyan Dimov <s.dimov@codelogic.eu>
2017-04-19 15:06:55 +0200

    Add a .NET core config example with Json file

---
+++ src/core.config/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+
+namespace core.config
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var config = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile("appsettings.local.json", optional: false, reloadOnChange: true) // overrides
+				//.AddEnvironmentVariables()
+				.Build();
+
+			Console.WriteLine($"App: {config["General:AppName"]}");
+			Console.WriteLine($"Env: {config["General:Environment"]}");
+		}
+	}
+}
